e91d1e74505638f777970fcacc26a28bd8f3fb53
Carlos Reyes <crrmagadan@icloud.com>
2024-06-22 15:47:01 -0400

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Carlos Reyes on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
