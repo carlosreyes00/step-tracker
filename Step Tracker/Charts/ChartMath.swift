17f3097a8563c3d16500192ad0d89a31d2fb5232
Carlos Reyes <crrmagadan@icloud.com>
2025-05-22 11:52:09 -0400

    pie chart basic ui

--- Step Tracker/Charts/ChartMath.swift
+++ Step Tracker/Charts/ChartMath.swift
@@ -13,7 +13,6 @@ struct ChartMath {
     static func averageWeekdayCount(for metric: [HealthMetric]) -> [WeekdayChartData] {
         
         let sortedByWeekday = metric.sorted { $0.date.weekdayInt < $1.date.weekdayInt }
-        
         let weekdayArray = sortedByWeekday.chunked { $0.date.weekdayInt == $1.date.weekdayInt }
         
         var weekdayChartData: [WeekdayChartData] = []
@@ -24,16 +23,6 @@ struct ChartMath {
             let avgSteps = total/Double(array.count)
             
             weekdayChartData.append(.init(date: firstValue.date, value: avgSteps))
-        }
-        
-        for metric in sortedByWeekday {
-            print("Day: \(metric.date.weekdayInt), value: \(metric.value)")
-        }
-        
-        print("-------")
-        
-        for day in weekdayChartData {
-            print("Day: \(day.date.weekdayInt), value: \(day.value)")
         }
         
         return weekdayChartData
