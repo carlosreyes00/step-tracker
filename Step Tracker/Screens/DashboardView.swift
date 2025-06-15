dad63bd95caf0276fd734b099e061853a974285e
Carlos Reyes <crrmagadan@icloud.com>
2025-06-15 14:36:44 -0400

    implement alert ui

--- Step Tracker/Screens/DashboardView.swift
+++ Step Tracker/Screens/DashboardView.swift
@@ -27,6 +27,9 @@ struct DashboardView: View {
     @Environment(HealthKitManager.self) private var hkManager
     @State private var isShowingPermissionPrimingSheet = false
     @State private var selectedStat: HealthMetricContext = .steps
+    @State private var isShowingAlert = false
+    @State private var fetchError: STError = .noData
+    
     var isSteps: Bool { selectedStat == .steps}
     
     var body: some View {
@@ -66,9 +69,11 @@ struct DashboardView: View {
                 } catch STError.authNotDetermined {
                     isShowingPermissionPrimingSheet = true
                 } catch STError.noData {
-                    print("❌ No Data Error")
+                    fetchError = .noData
+                    isShowingAlert = true
                 } catch {
-                    print("❌Unable to complete request")
+                    fetchError = .unableToCompleteRequest
+                    isShowingAlert = true
                 }
             }
             .navigationTitle("Dashboard")
@@ -83,6 +88,12 @@ struct DashboardView: View {
                 content: {
                     HealthKitPermissionPrimingView()
                 })
+            .alert(isPresented: $isShowingAlert, error: fetchError) { fetchError in
+                // Action
+            } message: { fetchError in
+                Text(fetchError.failureReason)
+            }
+
         }
         .tint(isSteps ? .pink : .indigo)
     }
