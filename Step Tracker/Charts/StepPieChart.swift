d9a1e53ce341537f73596a39bbc7d5a3b51b64d8
Carlos Reyes <crrmagadan@icloud.com>
2025-06-15 15:16:20 -0400

    implement haptic feedback

--- Step Tracker/Charts/StepPieChart.swift
+++ Step Tracker/Charts/StepPieChart.swift
@@ -11,6 +11,8 @@ import Charts
 struct StepPieChart: View {
     
     @State private var rawSelectedChartValue: Double? = 0
+    @State private var selectedDay: Date?
+    
     var chartData: [WeekdayChartData]
     
     var selectedWeekday: WeekdayChartData? {
@@ -79,6 +81,13 @@ struct StepPieChart: View {
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+        .sensoryFeedback(.selection, trigger: selectedDay)
+        .onChange(of: selectedWeekday) { oldValue, newValue in
+            guard let oldValue, let newValue else { return }
+            if oldValue.date.weekdayInt != newValue.date.weekdayInt {
+                selectedDay = newValue.date
+            }
+        }
     }
 }
 
